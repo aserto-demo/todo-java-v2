c8147d5e85847f3f77eb6b9de8a18dfe55068506
Bogdan Irimie <bogdan.irimie@aserto.com>
2024-01-25 11:03:31 +0200

    Create database schema on application start if it does not exist.

--- src/main/java/com/aserto/store/DataSourceConfig.java
+++ src/main/java/com/aserto/store/DataSourceConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
-import java.util.Properties;
 
 @Configuration
 public class DataSourceConfig {
@@ -15,13 +14,6 @@ public class DataSourceConfig {
         final DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.sqlite.JDBC");
         dataSource.setUrl("jdbc:sqlite:mydb.db");
-
-        Properties properties = new Properties();
-        properties.setProperty("spring.jpa.database-platform", "org.hibernate.community.dialect.SQLiteDialect");
-        properties.setProperty("hibernate.hbm2ddl.auto", "update");
-        properties.setProperty("hibernate.show_sql", "true");
-
-        dataSource.setConnectionProperties(properties);
 
         return dataSource;
     }
